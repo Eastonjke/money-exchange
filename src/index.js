afc8fd95a56311287a7bf3196311ec2e1f69fc89
Denis Sholomitsky <subaru1634@mail.ru>
2019-03-25 10:04:32 +0300

    feat: add new function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+exchange = {};
+if(currency >10000){throw new Error( "You are rich, my friend! We don't have so much coins for exchange")}
+if(currency > 49){
+  exchange.H = Math.trunc(currency / 50);
+  currency = currency - (exchange.H * 50);
+}
+if(currency > 24){
+  exchange.Q = Math.trunc(currency / 25);
+  currency = currency - (exchange.Q * 25);
+}
+if(currency > 9){
+  exchange.D = Math.trunc(currency / 10);
+  currency = currency - (exchange.D * 10);
+}
+if(currency > 4){
+  exchange.N = Math.trunc(currency / 5);
+  currency = currency - (exchange.N * 5);
+}
+if(currency > 0){
+  exchange.P = currency;
+
+}
+return exchange;
 }
